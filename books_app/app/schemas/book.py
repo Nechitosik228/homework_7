b2197453bf3f9f9c5de408df3d692ab27113d04e
Nechitosik228 <nikitanechitailo93@gmail.com>
2024-10-18 15:03:37 +0200

    ...

--- books_app/app/schemas/book.py
+++
@@ -1,9 +0,0 @@
-from pydantic import BaseModel,Field
-
-
-
-class CreateBook(BaseModel):
-    name:str = Field(max_length=100)
-    author:str = Field(min_length=5,max_length=50)
-    release_year:int = Field(le=2024)
-    books_quantity:int = Field(gt=0)
--- books_app/db/models/__init__.py
+++
@@ -1,2 +0,0 @@
-from .config import Config
-from .book import Book
